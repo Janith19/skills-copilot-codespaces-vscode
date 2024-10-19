78c083adc6316b76042d94ffe7a0dbe2d4668049
Janith Ranasinghe <105631657+Janith19@users.noreply.github.com>
2024-10-19 13:54:55 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,37 +1,19 @@
 // Create a Web Server
-// Create a Web Server that listen to the port 3000
-const http = require('http');
-const fs = require('fs');
-const url = require('url');
-const comments = require('./comments.js');
-const querystring = require('querystring');
+const express = require("express");
+const app = express();
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  const path = url.parse(req.url).pathname;
-  console.log('path:', path);
-  if (path === '/comments') {
-    if (req.method === 'GET') {
-      const data = comments.getComments();
-      res.end(data);
-    } else if (req.method === 'POST') {
-      let body = '';
-      req.on('data', (chunk) => {
-        body += chunk;
-      });
-      req.on('end', () => {
-        const post = querystring.parse(body);
-        comments.addComment(post.comment);
-        res.end('Comment added');
-      });
-    }
-  } else {
-    res.writeHead(404);
-    res.end('Not Found');
-  }
+// Create a route for the home page
+app.get("/", (req, res) => {
+  res.send("Welcome to the home page!");
 });
 
-server.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+// Create a route for the comments page
+app.get("/comments", (req, res) => {
+  res.send("Welcome to the comments page!");
 });
-// Create a new file called comments.js
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
